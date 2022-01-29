2c7d1e2ca5809398c9ee745531d453d6c8fc6407
Sathya <nanasathya@gmail.com>
2022-01-29 17:15:44 +0530

    Created Level class which updates the level map with tile and draws it

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame, sys
 from settings import *
+from levels import Level
 
 # Pygame setup
 pygame.init()
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
+level = Level(level_map, screen)
 
 while True:
     for event in pygame.event.get():
@@ -13,6 +15,7 @@ while True:
             sys.exit()
     
     screen.fill("black")
+    level.run()
 
     pygame.display.update()
     clock.tick(60)
