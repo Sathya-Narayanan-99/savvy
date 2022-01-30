064ae84cd8fe7179d075f0bd4bbf29dd45439348
Sathya <nanasathya@gmail.com>
2022-01-31 00:24:02 +0530

    Added icon movement

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from overworld import Overworld
 class Game:
     def __init__(self):
         self.max_level = 3
-        self.overworld = Overworld(0, self.max_level, screen)
+        self.overworld = Overworld(1, self.max_level, screen)
 
     def run(self):
         self.overworld.run()
--- overworld.py
+++ overworld.py
@@ -2,10 +2,12 @@ import pygame
 from game_data import levels
 
 class Node(pygame.sprite.Sprite):
-    def __init__(self, pos, is_available):
+    def __init__(self, pos, is_available, icon_speed):
         super().__init__()
         self.image = pygame.Surface((100, 80))
         self.rect = self.image.get_rect(center = pos)
+
+        self.icon_detection_zone = pygame.Rect(self.rect.centerx - icon_speed //2, self.rect.centery - icon_speed //2, icon_speed, icon_speed)
         
         if is_available:
             self.image.fill('red')
@@ -15,9 +17,13 @@ class Node(pygame.sprite.Sprite):
 class Icon(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
+        self.position = pos
         self.image = pygame.Surface((20,20))
         self.image.fill('blue')
         self.rect = self.image.get_rect(center = pos)
+
+    def update(self):
+        self.rect.center = self.position
 
 class Overworld:
     def __init__(self, start_level, max_level, surface):
@@ -26,6 +32,11 @@ class Overworld:
         self.display_surface = surface
         self.max_level = max_level
         self.current_level = start_level
+
+        # icon movement
+        self.is_moving = False
+        self.movement_direction = pygame.math.Vector2(0,0)
+        self.speed = 8
 
         # sprites
         self.setup_nodes()
@@ -36,9 +47,9 @@ class Overworld:
         for index, node_data in enumerate(levels.values()):
 
             if index < self.max_level:
-                sprite = Node(node_data['node_pos'], True)
+                sprite = Node(node_data['node_pos'], True, self.speed)
             else:
-                sprite = Node(node_data['node_pos'], False)
+                sprite = Node(node_data['node_pos'], False, self.speed)
             
             self.nodes_sprite.add(sprite)
 
@@ -52,7 +63,44 @@ class Overworld:
         sprite = Icon(position)
         self.icon_sprite.add(sprite)
 
+    def get_input(self):
+        keys = pygame.key.get_pressed()
+        if not self.is_moving:
+            if keys[pygame.K_RIGHT] and self.current_level < self.max_level - 1:
+                self.movement_direction = self.get_movement_data('next')
+                self.current_level += 1
+                self.is_moving = True
+
+            elif keys[pygame.K_LEFT] and self.current_level > 0:
+                self.movement_direction = self.get_movement_data('prev')
+                self.current_level -= 1
+                self.is_moving = True
+    
+    def get_movement_data(self, direction):
+        pos = self.nodes_sprite.sprites()[self.current_level].rect.center
+        start = pygame.math.Vector2(pos)
+        
+        if direction == 'next':
+            pos = self.nodes_sprite.sprites()[self.current_level + 1].rect.center
+        else:
+            pos = self.nodes_sprite.sprites()[self.current_level - 1].rect.center
+        end = pygame.math.Vector2(pos)
+
+        return (end - start).normalize()
+
+    def update_icon(self):
+        if self.is_moving and self.movement_direction:
+            self.icon_sprite.sprite.position += self.movement_direction * self.speed
+            target_node = self.nodes_sprite.sprites()[self.current_level]
+
+            if target_node.icon_detection_zone.collidepoint(self.icon_sprite.sprite.position):
+                self.is_moving = False
+                self.movement_direction = pygame.math.Vector2(0,0)
+
     def run(self):
+        self.get_input()
         self.nodes_sprite.draw(self.display_surface)
+        self.icon_sprite.update()
         self.draw_paths()
+        self.update_icon()
         self.icon_sprite.draw(self.display_surface)
